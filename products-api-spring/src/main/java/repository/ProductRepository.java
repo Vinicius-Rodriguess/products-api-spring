c1b0858c4879a12e18065504fb4d04e6e29829f0
Vinicius-Rodriguess <rodrigues.vini.2004@gmail.com>
2025-08-19 21:34:09 -0300

    Inicial commit do spring-study com products-api-spring

---
+++ products-api-spring/src/main/java/repository/ProductRepository.java
@@ -0,0 +1,11 @@
+package repository;
+
+import io.github.productsapi.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product, String> {
+
+    List<Product> findByName(String name);
+}
