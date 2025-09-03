5697b548d776bdfc3d8c374d3e608ea3938d69ed
Vinicius Rodrigues <rodrigues.vini.2004@gmail.com>
2025-09-03 20:56:03 -0300

    feat: Add TodoValidator and MailSender

--- arquiteturaspring/src/main/java/io/github/cursodsousa/arquiteturaspring/todos/TodoService.java
+++ arquiteturaspring/src/main/java/io/github/cursodsousa/arquiteturaspring/todos/TodoService.java
@@ -1,23 +1,29 @@
 package io.github.cursodsousa.arquiteturaspring.todos;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TodoService {
 
     private TodoRepository repository;
+    private TodoValidator validator;
+    private MailSender mailSender;
 
-    public TodoService(TodoRepository todoRepository) {
+    public TodoService(TodoRepository todoRepository, TodoValidator validator, MailSender mailSender) {
         this.repository = todoRepository;
+        this.validator = validator;
+        this.mailSender = mailSender;
     }
 
     public TodoEntity salvar(TodoEntity novoTodo) {
+        validator.validar(novoTodo);
         return this.repository.save(novoTodo);
     }
 
     public void atualizarStatus(TodoEntity todo) {
         repository.save(todo);
+        String status = todo.getConcluido() == Boolean.TRUE ? "Concluido" : "Não Concluido";
+        mailSender.enviar("Todo " + todo.getDescricao() + " foi atualizado para " + status);
     }
 
     public TodoEntity buscarPorId(Integer id) {
