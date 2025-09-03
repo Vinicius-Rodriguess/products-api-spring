65013bab85957364e37b6ef1a6056908f23db3ed
Vinicius Rodrigues <rodrigues.vini.2004@gmail.com>
2025-09-02 21:11:00 -0300

    feat: Add Controller and Service todos

---
+++ arquiteturaspring/src/main/java/io/github/cursodsousa/arquiteturaspring/todos/TodoService.java
@@ -0,0 +1,26 @@
+package io.github.cursodsousa.arquiteturaspring.todos;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TodoService {
+
+    private TodoRepository repository;
+
+    public TodoService(TodoRepository todoRepository) {
+        this.repository = todoRepository;
+    }
+
+    public TodoEntity salvar(TodoEntity novoTodo) {
+        return this.repository.save(novoTodo);
+    }
+
+    public void atualizarStatus(TodoEntity todo) {
+        repository.save(todo);
+    }
+
+    public TodoEntity buscarPorId(Integer id) {
+        return repository.findById(id).orElse(null);
+    }
+}
